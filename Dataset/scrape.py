703811ab1703eac046de1d64d97f7e15931932ae
Murali T S <mt788@cornell.edu>
2020-03-20 02:07:12 -0400

    March20Update

--- Dataset/scrape.py
+++ Dataset/scrape.py
@@ -7,55 +7,55 @@ import schedule
 import time
 # import datetime
 
-# def job():
+def job():
     # print("I'm working...")
-URL = 'https://www.mohfw.gov.in/'
-page = requests.get(URL)
-currentDT = datetime.now()
+    URL = 'https://www.mohfw.gov.in/'
+    page = requests.get(URL)
+    currentDT = datetime.now()
 
-today1 = datetime.today().strftime('%d%m%Y')
+    today1 = datetime.today().strftime('%d%m%Y')
 
-file = str(today1)+str(currentDT.hour + 10) + "out.txt"
-orig_stdout = sys.stdout
-f = open(file, 'w')
-sys.stdout = f
+    file = str(today1)+str(currentDT.hour + 10) + "out.txt"
+    orig_stdout = sys.stdout
+    f = open(file, 'w')
+    sys.stdout = f
 
-soup = BeautifulSoup(page.content, 'html.parser')
-A = [tag.extract() for tag in soup.find_all("tr")]
-for i in A:
-    print(i.get_text())
+    soup = BeautifulSoup(page.content, 'html.parser')
+    A = [tag.extract() for tag in soup.find_all("tr")]
+    for i in A:
+        print(i.get_text())
 
-sys.stdout = orig_stdout
-f.close()
+    sys.stdout = orig_stdout
+    f.close()
 
-f = open(file)
-line = f.readline()
-C = []
-while line:
-    if line != '\n':
-        C.append(line.rstrip())
+    f = open(file)
     line = f.readline()
+    C = []
+    while line:
+        if line != '\n':
+            C.append(line.rstrip())
+        line = f.readline()
 
-ANS = []
-i = 0
-while i<len(C):
-    ab = C[i:i+6]
-    ANS.append(ab)
-    # print(ab)
-    i = i+6
-df = pd.DataFrame(ANS[1:len(ANS)-1],columns=ANS[0])
-print(df)
+    ANS = []
+    i = 0
+    while i<len(C):
+        ab = C[i:i+6]
+        ANS.append(ab)
+        # print(ab)
+        i = i+6
+    df = pd.DataFrame(ANS[1:len(ANS)-1],columns=ANS[0])
+    print(df)
 
-today1 = datetime.today().strftime('%d%m%Y')
-file = str(today1) +str(currentDT.hour + 10)+ "COVIDIND.csv"
-df.to_csv(file,index = False)
+    today1 = datetime.today().strftime('%d%m%Y')
+    file = str(today1) +str(currentDT.hour + 10)+ "COVIDIND.csv"
+    df.to_csv(file,index = False)
 
 # schedule.every(1).minutes.do(job)
-# schedule.every(24).hours.do(job)
-# schedule.every(1).day.at("10:30").do(job)
+# schedule.every(7).hours.do(job)
+schedule.every(1).day.at("07:00").do(job)
 
-# while 1:
-#     schedule.run_pending()
-#     time.sleep(1)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
 
 # job()
